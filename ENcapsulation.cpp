ea3b0b41b4d2ec3dd28ce98d2c093479c0e54045
Saurabh <165586434+Verma12334@users.noreply.github.com>
2024-04-01 15:20:18 +0530

    Add files via upload

---
+++ ENcapsulation.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class student{
+    string name;
+    int age;
+    int height; 
+
+    int getage(){
+        return this->age;
+    }
+};
+
+int main(){
+
+}
